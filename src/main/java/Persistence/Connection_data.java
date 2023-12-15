85b7dd9129234f528c679b1bcb293e9c0f0c78de
Nguyen-van-toan <toanvippk115@gmail.com>
2023-12-15 20:52:46 +0700

    new folder persistence

---
+++ src/main/java/Persistence/Connection_data.java
@@ -0,0 +1,30 @@
+package Persistence;
+import java.sql.*;
+
+import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.SQLException;
+  // Specify java.sql.Connection to avoid conflicts
+
+public class Connection_data  {
+
+    public static Connection  getConnection() {
+    	Connection con = null;
+    try {
+    String driver = "com.mysql.cj.jdbc.Driver";
+    String url = "jdbc:mysql://localhost:3306/shop";
+    String usename  = "toan";
+    String password = "123456789";
+    Class.forName(driver);
+
+     con = DriverManager.getConnection(url, usename, password);
+    System.err.println("success: " );
+    
+	} catch (Exception e) {
+	    e.printStackTrace();
+	}
+       return con;
+    }
+    
+
+}
