1627c8639ef5c1c83d86fb7a41fa99949bf505e3
Nguyen-van-toan <toanvippk115@gmail.com>
2023-12-18 23:50:14 +0700

    fix imp

---
+++ src/main/java/Persistence/DAO/Impl/ImpCategoryDAO.java
@@ -0,0 +1,19 @@
+package Persistence.DAO.Impl;
+
+import Persistence.DAO.ICategoryDAO;
+
+public class ImpCategoryDAO implements ICategoryDAO{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dell() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/Persistence/DAO/Impl/ImpCustommerDAO.java
+++ src/main/java/Persistence/DAO/Impl/ImpCustommerDAO.java
@@ -38,4 +38,62 @@ public class ImpCustommerDAO implements ICustommerDAO {
         return null;
 
 }
+	@Override
+	public void updatePasswordCustomer(String email, String password, String newPassword) {
+		try {
+			conn= new Connection_data().getConnection();
+//			String query = "select email, password from shop.customer where email =? AND Password =?";
+			String update = "UPDATE shop.customer SET Password = ? where email =? AND Password =?";
+			pr=conn.prepareStatement(update);
+			pr.setString(1, newPassword);
+			pr.setString(2, email);
+			pr.setString(3, password);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+	@Override
+	public void deleteCustomer(String email, String password) {
+		try {
+			conn= new Connection_data().getConnection();
+//			String query = "select email, password from shop.customer where email =? AND Password =?";
+			String update = "DELETE FROM shop.customer WHERE  where email =? AND Password =?";
+			pr=conn.prepareStatement(update);
+			
+			pr.setString(1, email);
+			pr.setString(2, password);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+	}
+	@Override
+	public void newCustommer( String username, String password, String email, String firstname, String city, String address, String state, String country, String created_at,
+			String lastname) {
+		try {
+			conn= new Connection_data().getConnection();
+//			String query = "select email, password from shop.customer where email =? AND Password =?";
+			String update = "INSERT INTO shop.customer ( first_name, last_name, email, address, city, state, country, created_at , password) VALUES (?,?,?,?,?,?,?,?,?)"
+					;
+			pr=conn.prepareStatement(update);
+			pr.setString(1, firstname);
+			pr.setString(2, lastname);
+			pr.setString(3, email);
+			pr.setString(4, address);
+			pr.setString(5, city);
+			pr.setString(6, state);
+			pr.setString(7, country);
+			pr.setString(8, created_at);
+			pr.setString(9, password);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+	}
 }
