b8d1b263c1f7d018f6ac33a466602e7eb01376c2
Nguyen-van-toan <toanvippk115@gmail.com>
2023-12-19 00:00:30 +0700

    fix imp_2

---
+++ src/main/java/Persistence/DAO/Impl/imProductDAO.java
@@ -0,0 +1,25 @@
+package Persistence.DAO.Impl;
+
+import Persistence.DAO.IProductDAO;
+
+public class imProductDAO implements IProductDAO{
+
+    @Override
+    public void newProduct() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'newProduct'");
+    }
+
+    @Override
+    public void updateProduct() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateProduct'");
+    }
+
+    @Override
+    public void deleteProduct() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteProduct'");
+    }
+
+}
---
+++ src/main/java/Persistence/DAO/Impl/impOrderDAO.java
@@ -0,0 +1,19 @@
+package Persistence.DAO.Impl;
+
+import Persistence.DAO.IOrderDAO;
+
+public class impOrderDAO implements IOrderDAO{
+
+    @Override
+    public void updateItem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateItem'");
+    }
+
+    @Override
+    public void deleteItem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteItem'");
+    }
+
+}
